fae31d2d9c882e8e4510a540dcebe9f75d2f278d
LisPOLSKA <jankuziok@gmail.com>
2024-10-12 21:53:27 +0200

    test

--- api/firebase.js
+++ api/firebase.js
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 import { readFileSync } from "fs";
 
 // Initialize Firebase Admin SDK with service account
-const serviceAccount = JSON.parse(readFileSync("./firebase-service-account.json", "utf-8"));
+const serviceAccount = JSON.parse(readFileSync("./firebase-service-account.json.local", "utf-8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
